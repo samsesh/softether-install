2ce32a365ab6b656871532880a033b8bc00859c8
Sam Sesh <admin@samsesh.net>
2023-02-15 19:53:19 +0330

    edit install file

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#update repo
+apt update 
+clear
+#install figlet
+apt install -y figlet 
+
+clear
+
+#install softether requirement
+figlet installing requirement
+sleep 5
+apt install -y build-essential make gcc zlib1g-dev libreadline-dev libncurses-dev libssl-dev
+clear
+
+
+#install softether - start
+figlet installing softether
+# download softether
+wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+#extart file
+tar xzf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+# compiling softether
+cd vpnserver/
+make
+cd ..
+# move to new dir 
+mv vpnserver /usr/local
+cd /usr/local/vpnserver/
+# get file permission
+chmod 600 *
+chmod 700 vpnserver vpncmd
+# start server
+/usr/local/vpnserver/vpnserver start
+# enable ip4 forward
+echo net.ipv4.ip_forward = 1 | ${SUDO} tee -a /etc/sysctl.conf
+# create vpn server service
+sudo cat >> /lib/systemd/system/vpnserver.service << EOF
+[Unit]
+
+Description=SoftEther VPN Server
+
+After=network.target
+
+[Service]
+
+Type=forking
+
+ExecStart=
+
+ExecStart=/usr/local/vpnserver/vpnserver start
+
+ExecStop=/usr/local/vpnserver/vpnserver stop
+
+[Install]
+
+WantedBy=multi-user.target
+
+EOF
+systemctl enable vpnserver.service
+systemctl start vpnserver.service
+/usr/local/vpnserver/vpncmd
