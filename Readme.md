b03033de9686bbc4b978aa81eb1ea24e8475b227
Sam Sesh <admin@samsesh.net>
2023-04-14 16:00:55 +0330

    update

--- Readme.md
+++ Readme.md
@@ -1,23 +1,30 @@
-# softehter installer on ubuntu
+## softehter installer on ubuntu
 
 - one command installer
 ``` bash
-git clone https://github.com/samsesh/softether-install && cd softether-install &&  bash install.sh
+git clone https://github.com/samsesh/softether-install && cd softether-install &&  sh install.sh
 ```
 - install use tmux on one command 
 ``` bash
-tmux new -s softetherInstaller "git clone https://github.com/samsesh/softether-install && cd softether-install &&  bash install.sh"
+tmux -s softetherInstaller "git clone https://github.com/samsesh/softether-install && cd softether-install &&  sh install.sh"
 ```
-## fisrt run
-- start service 
+>  It is better to use tmux so that if the ssh connection with the server is interrupted, the command will not be broken, If the connection is interrupted, you can return to the installation page with the following command after connecting to the server
+```bash
+tmux a -d -t softether
+```
+## cfwarp
+- We use cfwarp so that we are inside the cloudflare network and the IP address of the server is not leaked and remains clean.
+- To use it, just do the following
+1. clone project :
+``` bash 
+git clone https://github.com/samsesh/softether-install.git
+```
+2. Go to the project directory :
+``` bash 
+cd softether-install
+```
+3. run cfwarp.sh 
 ``` bash
-service vpnserver start
+bash cfwarp.sh 
 ```
-- now run
-``` bash
-cd /usr/local/vpnserver/ && ./vpncmd
-```
-- and select ro service then for set password on use this command on 
-``` bash
-ServerPasswordSet
-```
+> Note: This feature is not recommended for more than 10-20 users and it causes a decrease in speed - [cfwarp repository link](https://gitlab.com/rwkgyg/CFwarp/)
--- install.sh
+++ install.sh
@@ -1,72 +1,88 @@
 #!/bin/bash
+check_if_running_as_root() {
+    # If you want to run as another user, please modify $EUID to be owned by this user
+    if [[ "$EUID" -ne '0' ]]; then
+        echo "$(tput setaf 1)Error: You must run this script as root!$(tput sgr0)"
+        exit 1
+    else
+        echo $(tput setaf 2)pritunl installing starting ...$(tput sgr0)
+    fi
+}
 
-#update repo
-apt update 
-clear
-#install figlet
-apt install -y figlet 
+check_os() {
+    # Get the operating system name and version
+    OS=$(lsb_release -si)
+    VERSION=$(lsb_release -sr)
 
-clear
+    # Check if the OS is Ubuntu 20.04
+    if [ "$OS" != "Ubuntu" ] || [ "$VERSION" != "20.04" ]; then
+        echo "This script only works on Ubuntu 20.04"
+        exit 1
+    fi
+}
+sysup() {
+    bash <(curl -s https://raw.githubusercontent.com/samsesh/Ubuntu-Optimizer/main/ubuntu-optimizer.sh)
+}
 
-#install softether requirement
-figlet installing requirement
-sleep 5
-apt install -y build-essential make gcc zlib1g-dev libreadline-dev libncurses-dev libssl-dev
-clear
-figlet optimizer os
-bash <(curl -s https://raw.githubusercontent.com/samsesh/Ubuntu-Optimizer/main/ubuntu-optimizer.sh)
-clear
+req() {
+    #install softether requirement
+    apt install -y figlet
+    clear
+    figlet installing requirement
+    sleep 5
+    apt install -y build-essential make gcc zlib1g-dev libreadline-dev libncurses-dev libssl-dev
+    clear
+}
 
-#install softether - start
-figlet installing softether
-# download softether
-wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
-#extart file
-tar xzf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
-# create vpn server service
-cp vpnserver.service /lib/systemd/system/
-# compiling softether
-cd vpnserver/
-make
-cd ..
-clear
-# move to new dir 
-mv vpnserver /usr/local
-cd /usr/local/vpnserver/
-# get file permission
-chmod 600 *
-chmod 700 vpnserver vpncmd
-# start server
-/usr/local/vpnserver/vpnserver start
-# enable ip4 forward
-echo net.ipv4.ip_forward = 1 | ${SUDO} tee -a /etc/sysctl.conf
+install() {
+    #install softether - start
+    figlet installing softether
+    # download softether
+    wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+    #extart file
+    tar xzf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+    # compiling softether
+    cd vpnserver/
+    make
+    cd ..
+    clear
+    # move to new dir
+    mv vpnserver /usr/local
+    cd /usr/local/vpnserver/
+    # get file permission
+    chmod 600 *
+    chmod 700 vpnserver vpncmd
+    # start server
+    /usr/local/vpnserver/vpnserver start
+    # enable ip4 forward
+    ln -s /usr/local/vpnserver/vpnserver /usr/bin/vpnserver
+    ln -s /usr/local/vpnserver/vpncmd /usr/bin/vpncmd
+    echo net.ipv4.ip_forward = 1 | ${SUDO} tee -a /etc/sysctl.conf
+    # create vpn server service
+    cp vpnserver.service /lib/systemd/system/
+    # start vpn server service
+    systemctl start vpnserver.service
+    # add to start up vpn server service
+    systemctl enable vpnserver.service
 
-# add short command
-echo "alias vpnserver="/usr/local/vpnserver/vpnserver"" >>~/.bashrc
-echo "alias vpncmd="/usr/local/vpnserver/vpncmd"" >>~/.bashrc
+    /usr/local/vpnserver/vpncmd
 
-# start vpn server service
-systemctl start vpnserver.service
-# add to start up vpn server service
-systemctl enable vpnserver.service
+    clear
+    figlet finish
+    echo "samsesh"
+    #install softether - end
 
+    echo "now run vpncmd and select ro service then for set password on use this command on vpncmd ServerPasswordSet"
 
+    sleep 5
 
-clear
-figlet finish
-echo "samsesh"
-#install softether - end
+    echo "check your service status"
+    echo "systemctl status vpnserver.service"
+}
 
-
-
-sleep 5
-
-echo "check your service status"
-sleep 5
-systemctl restart vpnserver.service
-systemctl status vpnserver.service
-sleep 5
-clear
-echo "now run /usr/local/vpnserver/vpncmd and select ro service then for set password on use this command on /usr/local/vpnserver/vpncmd ServerPasswordSet"
-sleep 15
-/usr/local/vpnserver/vpncmd
+#run
+check_if_running_as_root
+check_os
+sysup
+req
+install
